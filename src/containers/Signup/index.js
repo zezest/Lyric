6add18f318a3993bbc173505979652eedf74f031
shlee92 <leesanghoon@leeSanghoons-MacBook-Pro.local>
2017-08-07 13:50:06 +0900

    회원가입 폼 추가

--- src/containers/Signup/index.js
+++ src/containers/Signup/index.js
@@ -3,21 +3,69 @@ import React, { Component } from 'react';
 import Input from '../../components/TextField/input';
 
 import {
-  SignupForm
+  SignupForm, Submit
 } from './styled';
 
 export default class Signup extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      name: '',
+      email: '',
+      password: '',
+      password_confirm: '',
     }
   }
 
+  onChangeToState = e => {
+    const { name, value } = e.target;
+
+    this.setState({
+      [name]: value
+    })
+  }
+
+  validate = () => {
+    const { name, email, password, password_confirm } = this.state;
+
+    if (email === '') {
+      alert('이메일을 입력해주세요.');
+      return false;
+    }
+
+    if (password === '' || password_confirm === '') {
+      alert('비밀번호를 입력해주세요.');
+      return false;
+
+    } else if (password !== password_confirm) {
+      alert('비밀번호가 같지 않습니다.');
+      return false;
+    }
+
+    if (name === '') {
+      alert('이름을 입력해주세요.');
+      return false;
+    }
+
+    return true;
+  }
+
+  onSubmit = e => {
+    e.preventDefault();
+    if (!this.validate()) return;
+  }
+
   render() {
+    const { name, email, password, password_confirm } = this.state;
     return (
-      <div>
-        <p>Main</p>
-      </div>
+      <SignupForm onSubmit={this.onSubmit}>
+        <h1>Sign-Up</h1>
+        <Input type="email" name="email" value={email} onChange={this.onChangeToState} autoComplete="off" title="Email" hint="lyrics@lyrics.com" />
+        <Input type="password" name="password" value={password} onChange={this.onChangeToState} autoComplete="off" title="Password" />
+        <Input type="password" name="password_confirm" value={password_confirm} onChange={this.onChangeToState} autoComplete="off" title="password confirm" />
+        <Input type="text" name="name" value={name} onChange={this.onChangeToState} autoComplete="off" title="Name" hint="홍길동" />
+        <Submit>Submit</Submit>
+      </SignupForm>
     )
   }
 }
--- src/containers/Signup/styled.js
+++ src/containers/Signup/styled.js
@@ -1,6 +1,22 @@
 import styled from 'styled-components';
 
 export const SignupForm = styled.form`
-  width: 500px;
+  width: 400px;
   margin: 0 auto;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`
+
+export const Submit = styled.button.attrs({
+  type: "submit",
+})`
+  width: 100%;
+  height: 50px;
+  color: #fff;
+  font-size: 16px;
+  border: 0;
+  background-color: #483D8B;
+  cursor: pointer;
 `
