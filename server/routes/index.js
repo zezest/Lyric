8176f82d3010381eb89b42d1d5e0bc04828b723c
eunsoo88kim <eskim@station3.co.kr>
2017-08-20 17:04:27 +0900

    메일보내기 인증 추가

--- server/routes/index.js
+++ server/routes/index.js
@@ -21,8 +21,7 @@ module.exports = app => {
   // // FIND LYRICS BY TITLE
   // app.get('/api/lyrics/title/:title', lyricController.getSearchLyrics);
   // EMAIL SEND
-  app.post('/api/lyrics/send', lyricController.sendLyric);
-  // app.post('/api/lyrics/send', isAuthenticated, lyricController.sendLyric);
+  app.post('/api/lyrics/send', isAuthenticated, lyricController.sendLyric);
   // CREATE LYRIC
   app.post('/api/lyrics', isAuthenticated, lyricController.postCreateLyric);
   // UPDATE THE LYRIC
